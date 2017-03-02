f78ae304209ea783e854e7c2b5449d738a85f160
JohnnyB0Y <JohnnyB0Y@MacBook-Pro.local>
2017-03-02 17:41:36 +0800

    初始化项目

---
+++ AGSortedMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AGSortedMan
+//
+//  Created by JohnnyB0Y on 2017/3/2.
+//  Copyright © 2017年 JohnnyB0Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AGSortedMan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AGSortedMan
+//
+//  Created by JohnnyB0Y on 2017/3/2.
+//  Copyright © 2017年 JohnnyB0Y. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
